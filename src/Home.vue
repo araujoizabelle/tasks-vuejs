cc0303e01fc05dbb4abd56b522f1ff48f37b9a81
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-05-17 21:55:28 -0300

    feat: create vuejs basics

---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>home</p>
+</template>
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
